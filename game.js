5e5af04844d040768c736f6228d19f9ba080396a
Charles Brandt <luftbuefel@gmail.com>
2019-02-07 19:19:30 -0800

    fixing some bugs with commands

--- game.js
+++ game.js
@@ -1,5 +1,9 @@
 //GAME ENGINE CODE HERE
 //game-wide commands
+//save the keycode for enter
+const ENTER = 13;
+const NONE = "";
+
 const NORTH="north";
 const SOUTH="south";
 const EAST="east";
@@ -8,44 +12,50 @@ const DIRECTIONS = [NORTH, SOUTH, EAST, WEST];
 const HELP ="help";
 const LOOK = "look";
 const INVENTORY = "inventory";
+
 //UI controls
 const textDisplay = window.document.getElementById("gameDisplay");
 const actionButton = window.document.getElementById("actionButton");
 actionButton.onclick = performCommand;
 const actionText = window.document.getElementById("actionText");
 actionText.onkeypress = catchKeypress;
-// actionText.onKeyPress  = catchKeypress;
+
 //used for computer generated speech
 const synthesis = window.speechSynthesis;
+
 //keeps track of where the player is
 let currentLocation;
 
 
 function startGame(){
+  textDisplay.value="";
   welcome();
 }
 
 
 function catchKeypress(e){
-  console.log("a key was pressed");
+  if(e.keyCode==ENTER){
+    e.preventDefault();
+    performCommand();
+  }
 }
 
 
 function performCommand(){
-  // console.log(JSON.stringify(actionText));
   //extract the command text and validate it
   let command = validateCommand(actionText.value);
+  console.log("command "+command);
   //erase the old text from the action text area
   actionText.value = "";
   //do the command at the currentLocation
-  currentLocation(command);
+  currentLocation(command, false);
 }
 
 
-function write(textMessage, speakText = false){
+function write(textMessage, speakText = true){
   textDisplay.value+=textMessage+"\n\n";
-  //set the textDisplay scroll position to the bottom so that the new text will show up
-  // textDisplay.scroll()
+  //move the scrollbar to the bottom so the new text is visible
+  textDisplay.scrollTop = textDisplay.scrollHeight;
   if(speakText){
     return;
     speak(textMessage);
@@ -100,7 +110,7 @@ function validateCommand(command){
 
 
 function checkCommand(command){
-  console.log(command);
+  // console.log(command);
   if(command==LOOK){
     currentLocation();
   } else if(DIRECTIONS.indexOf(command)>-1){
@@ -113,16 +123,13 @@ function checkCommand(command){
 }
 
 
-
 //Game specific code goes below here
 //
 //
 //locations
-
-
 function welcome(){
   currentLocation = welcome;
-  write("You recently won a trip on a gameshow. The trip was to fabulous Maui. Unfortunately your plane experienced some complications and crashed into the sea. Luckily you were able to hold on to your seat, using it as a floatation device. After several long hours you wash up on the beach of a mysterious island.", true)
+  write("You recently won a trip on a gameshow. The trip was to fabulous Maui. Unfortunately your plane experienced some complications and crashed into the sea. Luckily you were able to hold on to your seat, using it as a floatation device. After several long hours you wash up on the beach of a mysterious island.", false);
   beach();
 }
 
@@ -136,7 +143,7 @@ function beach(command = ""){
     case WEST:
       forest();
       break;
-    case "":
+    case NONE:
       write("You are standing on a sandy beach. Tiny animals skutter too and fro. A large looming mountain lies to the north. A friendly sea lion playfully splashes in the waves. There seems to be a path leading into the forest to the west.", true);
       break;
     default:
@@ -173,7 +180,7 @@ function abandonedVillage(){
       forest();
       break;
     case "":
-      write("Wow, it looks like6 you have arrived at an abandoned village. I bet there is some good stuff here!");
+      write("Wow, it looks like you have arrived at an abandoned village. I bet there is some good stuff here!");
       break;
     default:
       checkCommand(command);
@@ -183,10 +190,12 @@ function abandonedVillage(){
 
 
 function volcano(){
+// alert("volcano"+command);
+
   currentLocation = volcano;
   switch (command) {
-    case "":
-      setMessage("Be careful dude! This place is dangerous.");
+    case NONE:
+      // setMessage("Be careful dude! This place is dangerous.");
       write("Oh dang! You are at the top of a scary volcano.");
       break;
     case SOUTH:
@@ -204,7 +213,7 @@ function gameOver(){
 }
 
 
-// printVoices();
+printVoices();
 // speak("It is working");
 //start the actual game
 startGame();
